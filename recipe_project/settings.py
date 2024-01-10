95b4584b589ee70a512599c842b33a01d1f0c49e
ksope <105855785+ksope@users.noreply.github.com>
2024-01-10 20:12:18 +0000

    Check debug log files

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -157,7 +157,7 @@ DATABASES['default'].update(db_from_env)
 
 #cloudinary configuration
 cloudinary.config( 
-  cloud_name = "cloud_name", 
-  api_key = "api_key", 
-  api_secret = "api_secret" 
+  cloud_name = cloud_name, 
+  api_key = api_key, 
+  api_secret = api_secret 
 )
