aa8c6815ba9e0437786151171ae1c68b8579ccd0
ksope <105855785+ksope@users.noreply.github.com>
2024-01-11 17:13:52 +0000

    Fix issue with application not being able to read .env variables

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -163,5 +163,5 @@ DATABASES['default'].update(db_from_env)
 cloudinary.config( 
   cloud_name = CLOUDINARY_CLOUD_NAME, 
   api_key = CLOUDINARY_API_KEY, 
-  api_secret = CLOUDINARY_API_SECRE
+  api_secret = CLOUDINARY_API_SECRET
 )
