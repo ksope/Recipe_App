760879c238bf472906590e8d621b5901bd419dba
ksope <105855785+ksope@users.noreply.github.com>
2024-01-11 17:55:27 +0000

    Fix issue with application not being able to read .env variables

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -13,27 +13,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import cloudinary
-from decouple import config
-
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
+
 import os
-load_dotenv()
+from decouple import config
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','SECRET_KEY')
-
-CLOUDINARY_API_KEY = config('CLOUDINARY_API_KEY')
-CLOUDINARY_API_SECRET = config('CLOUDINARY_API_SECRET')
-CLOUDINARY_CLOUD_NAME = config('CLOUDINARY_CLOUD_NAME')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -159,9 +151,11 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
+CLOUDINARY_STORAGE = {
+    'cloud_name' : config('CLOUDINARY_CLOUD_NAME'),
+    'api_key' : config('CLOUDINARY_API_KEY'),
+    'api_secret' : config('CLOUDINARY_API_SECRET')
+}
+
 #cloudinary configuration
-cloudinary.config( 
-  cloud_name = CLOUDINARY_CLOUD_NAME, 
-  api_key = CLOUDINARY_API_KEY, 
-  api_secret = CLOUDINARY_API_SECRET
-)
+cloudinary.config(**CLOUDINARY_STORAGE)
