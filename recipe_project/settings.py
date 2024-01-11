b05df46b397e33878aae9ef7e21bba9c9cad57f7
ksope <105855785+ksope@users.noreply.github.com>
2024-01-11 17:56:17 +0000

    Fix issue with application not being able to read .env variables

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ from decouple import config
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY','SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
